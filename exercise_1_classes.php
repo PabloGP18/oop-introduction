9d60df8e64d281a8f5749dedd58fa16cd30b3efb
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-06-23 11:30:10 +0200

    exercise 1 done

--- exercise_1_classes.php
+++ exercise_1_classes.php
@@ -11,5 +11,41 @@ TODO: Make a getInfo function which returns "This beverage is <temperature> and 
 TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
  print the getInfo on the screen.
 TODO: Print the temperature on the screen.
-USE TYPEHINTING EVERYWHERE!
+USE TYPE HINTING EVERYWHERE!
 */
+
+class Beverage
+{
+    // The Properties
+    public $color;
+    public $price;
+    public $temperature;
+    public $name;
+
+
+// The constructure with incoming parameters in the brackets
+
+    public function __construct(string $color, float $price, string $name)
+    {
+        $this->color= $color;
+        $this->price= $price;
+        $this->temperature= "cold";
+        $this->name= $name;
+    }
+
+    /**
+     * @param mixed $temperature
+     */
+
+
+    // The functions
+    public function getInfoBeverage(): void
+    {
+        echo "this $this->name is $this->temperature and $this->color";
+    }
+}
+// For every object you want to create, you just need to instantiate a new object.
+$beverage = new beverage("black", 2,"cola");
+// Now that we created 3 animal objects. we can also start to use them in our code!
+$beverage->getInfoBeverage();
+
