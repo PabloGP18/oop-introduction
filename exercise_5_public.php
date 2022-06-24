af58668069b04ac473d56cda6d800f64bbb8776d
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-06-24 12:13:02 +0200

    exercise 5 done

--- exercise_5_public.php
+++ exercise_5_public.php
@@ -1,1 +1,56 @@
 <?php
+
+declare(strict_types=1);
+
+/* EXERCISE 5
+Copy the class of exercise 1.
+TODO: Change the properties to private.
+TODO: Fix the errors without using getter and setter functions.
+TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
+*/
+
+class Beverage
+{
+    // The Properties
+    private string $color;
+    private float $price;
+    private string $temperature;
+    private float $newPrice;
+
+
+// The constructure with incoming parameters in the brackets
+
+    public function __construct(string $color, float $price, string $temperature ="cold")
+    {
+        $this->color= $color;
+        $this->price= $price;
+        $this->temperature= $temperature;
+    }
+
+    /**
+     * @param mixed $temperature
+     */
+
+
+    // The functions
+    public function getInfoBeverage(): string
+    {
+        return "this beverage is $this->temperature and $this->color";
+    }
+
+    function printNewPrice (float $newPrice)
+    {
+        if($newPrice <= 0){
+            return "no free drinks for you!";
+        }
+        $this->price=$newPrice;
+        return "this beverage is $this->temperature and $this->color and $this->price euro";
+    }
+}
+// For every object you want to create, you just need to instantiate a new object.
+$beverage = new beverage("black", 2);
+// Now that we created 3 animal objects. we can also start to use them in our code!
+//$beverage->temperature="warm"; To change default string
+//$beverage->getInfoBeverage();
+echo $beverage->getInfoBeverage(); echo "<br>";
+echo $beverage->printNewPrice(0);
